0b79b4c1bb48bc8cf30908b204e0b7a9619fa695
Walter Vaughan <walter.vaughan@mines.sdsmt.edu>
2018-11-17 22:16:49 -0700

    added GPL License

--- render.sh
+++ render.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# render.sh -- abuses GNU Octave, ImageMagick, ffmpeg to render sin(nx)
+# Copyright (C) 2018  Walter B. Vaughan
+# 
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+# 
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along
+# with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 # set this to (at least) the number of processor cores on your system
 NUM_THREADS=6
 # total number of frames to render (will always be a full cycle of sin(x))
